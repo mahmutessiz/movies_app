d05b4078a7d7d6b92adc40f4d2082753e6562ae2
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-04-04 16:58:40 +0300

    add topseries section

--- movies_app/src/views/HomeView.vue
+++ movies_app/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeroSection from '@/components/HeroSection.vue'
 import TrendingWeek from '@/components/TrendingWeek.vue'
+import TopSeries from '../components/TopSeries.vue'
 </script>
 
 <template>
   <HeroSection />
+  <TopSeries />
   <TrendingWeek />
 </template>
