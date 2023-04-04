07ea4deb4473089876234f8b17291cb86cf44379
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-04-04 17:53:47 +0300

    change style of TopMovies section

--- movies_app/src/components/TopMovies.vue
+++ movies_app/src/components/TopMovies.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-full">
     <div class="w-full p-4"><h2 class="text-center text-2xl text-green-500">Top Movies</h2></div>
-    <ul class="flex w-full flex-wrap items-center justify-center gap-4 p-4">
-      <li v-for="n in 20" :key="n" class="h-80 w-80 bg-white">{{ n }}</li>
+    <ul class="flex w-full items-center gap-4 overflow-x-scroll p-4">
+      <li v-for="n in 20" :key="n" class="h-80 w-80 shrink-0 bg-white">{{ n }}</li>
     </ul>
   </div>
 </template>
