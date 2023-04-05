1f5961f6851d6685b9c2bfec788413da863d9b44
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-04-05 17:07:52 +0300

    add pinia store

--- movies_app/src/components/HeroSection.vue
+++ movies_app/src/components/HeroSection.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import axios from 'axios'
+import { onMounted, computed } from 'vue'
 //Swiper.js setup
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/swiper-bundle.css'
@@ -11,24 +10,14 @@ import 'swiper/css/pagination'
 import 'swiper/css/navigation'
 
 //Pinia setup
-/* import { storeToRefs } from 'pinia'
 import { useApiStore } from '../stores/api'
 
-const data = useApiStore()
-const refer = storeToRefs(data.movieList)
+const myMovieListData = useApiStore()
+
+myMovieListData.fetchMovieListData()
 
 onMounted(() => {
-  console.log(data.myMovieList(refer))
-}) */
-const denemeArr = ref([])
-const sourceOfMovies = 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/'
-const movieListUrl = import.meta.env.VITE_API_URL
-const movieListApiKey = import.meta.env.VITE_API_KEY
-
-onMounted(async () => {
-  await axios.get(`${movieListUrl}?api_key=${movieListApiKey}`).then((response) => {
-    denemeArr.value = response.data
-  })
+  computed(() => myMovieListData.myMovieList)
 })
 
 const modules = [Pagination, Navigation, Autoplay]
@@ -52,15 +41,15 @@ const modules = [Pagination, Navigation, Autoplay]
     class="mySwiper"
   >
     <swiper-slide
-      v-for="(data, index) in denemeArr.items"
+      v-for="(data, index) in myMovieListData.myMovieList.items"
       :virtualIndex="data.id"
       :key="data.id"
       class="bg-gradient-from-t relative flex flex-wrap items-center justify-center bg-gradient-to-b from-red-900 to-red-600 py-4 text-center"
     >
       <div class="w-full px-4 py-4">
-        <h2 class="text-left text-2xl font-bold text-green-500">Editor's pick #{{ index }}</h2>
+        <h2 class="text-left text-2xl font-bold text-green-500">Editor's pick #{{ index + 1 }}</h2>
       </div>
-      <img :src="sourceOfMovies + data.backdrop_path" class="w-40" alt="" />
+      <img :src="myMovieListData.sourceOfMovies + data.backdrop_path" class="w-40" alt="" />
       <p class="w-full">{{ data.original_title }}</p>
     </swiper-slide>
     <div class="absolute bottom-[10%] right-4 z-20 flex flex-col gap-4">
--- movies_app/src/stores/api.js
+++ movies_app/src/stores/api.js
@@ -1,8 +1,20 @@
-import { ref } from 'vue'
 import { defineStore } from 'pinia'
-
 import axios from 'axios'
 
-export const useApiStore = defineStore('api', () => {
-  return {}
+export const useApiStore = defineStore('api', {
+  state: () => ({
+    movieListApiKey: import.meta.env.VITE_API_KEY,
+    movieListApiUrl: import.meta.env.VITE_API_URL,
+    sourceOfMovies: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/',
+    myMovieList: []
+  }),
+  actions: {
+    async fetchMovieListData() {
+      await axios
+        .get(`${this.movieListApiUrl}?api_key=${this.movieListApiKey}`)
+        .then((response) => {
+          this.myMovieList = response.data
+        })
+    }
+  }
 })
