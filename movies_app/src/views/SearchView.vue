f519d00249f219b9c1719a62d5342d385a310ab9
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-04-13 15:41:17 +0300

    error handling

--- movies_app/src/views/SearchView.vue
+++ movies_app/src/views/SearchView.vue
@@ -23,29 +23,50 @@ const searchUrl =
   '&page=1&include_adult=false';
 
 const router = useRouter();
+let isFinish = ref(false);
 
 onMounted(async () => {
-  await axios.get(searchUrl).then((response) => {
-    searchData.value = response.data;
-  });
+  await axios
+    .get(searchUrl)
+    .then((response) => {
+      searchData.value = response.data;
+    })
+    .finally(() => {
+      isFinish.value = true;
+    });
 });
 let searchData = ref([]);
 </script>
 
 <template>
+  <!-- Page load animation -->
+  <div class="fixed inset-0 z-50" v-if="isFinish == false">
+    <div class="h-screen bg-black/90">
+      <div class="flex h-full items-center justify-center">
+        <img
+          class="w-full sm:w-1/2"
+          src="../assets/b1cfddae9e7b9645b9cde7ad9ee4f6bf.gif"
+          alt="loading"
+        />
+      </div>
+    </div>
+  </div>
+
+  <!-- Main -->
   <div class="mt-12 w-full">
     <div class="w-full p-4">
       <h2 class="text-xl font-bold text-green-500 sm:text-center">Results</h2>
     </div>
     <ul
       class="flex w-full items-center gap-4 overflow-x-scroll p-4 sm:flex-wrap sm:justify-center sm:overflow-hidden"
+      v-if="searchData.results != ''"
     >
       <li
         v-for="data in searchData.results"
         :key="data.id"
         class="w-40 shrink-0 rounded-lg bg-slate-800 text-center text-green-300 shadow-md shadow-rose-500 sm:w-60"
       >
-        <div v-if="data.poster_path">
+        <div>
           <img
             class="cursor-pointer"
             :src="sourceOfPosterUrl + data.poster_path"
@@ -57,11 +78,16 @@ let searchData = ref([]);
                 router.push(`/tv/${data.id}`);
               }
             "
+            v-if="data.poster_path"
           />
+          <p class="grid h-20 w-full place-items-center rounded-md bg-white text-red-500" v-else>
+            image not found
+          </p>
           <p class="h-16 p-2" v-if="data.title">{{ data.title }}</p>
           <p class="h-16 p-2" v-else>{{ data.name }}</p>
         </div>
       </li>
     </ul>
+    <div class="flex w-full justify-center" v-else>{{ movieName }} Not found</div>
   </div>
 </template>
