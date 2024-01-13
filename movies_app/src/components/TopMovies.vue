8948939ee19c653be625e609f0094af3765eda78
mahmutessiz <mahmutessiz71@gmail.com>
2024-01-13 13:00:10 +0300

    fix img load bug in top movies

--- movies_app/src/components/TopMovies.vue
+++ movies_app/src/components/TopMovies.vue
@@ -46,7 +46,8 @@ const changeButtonStyle = (id) => {
                 saveToLocalStorage(data.title, {
                   id: data.id,
                   title: data.title,
-                  media_type: data.media_type
+                  media_type: data.media_type,
+                  poster_path: data.poster_path
                 }),
                   changeButtonStyle(`btn-favorite-${data.id}`);
               }
