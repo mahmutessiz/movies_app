834e872a815f45075e06cc9183b2839e98eb8ee7
mahmutessiz <mahmutessiz71@gmail.com>
2024-01-13 11:21:05 +0300

    add fav buttons for movies page

--- movies_app/src/components/TrendingWeek.vue
+++ movies_app/src/components/TrendingWeek.vue
@@ -10,12 +10,17 @@ import 'swiper/css/navigation';
 
 //Pinia setup
 import { useApiStore } from '../stores/api';
-
+import { saveToLocalStorage } from '../utils/saveFavorite';
 const trendingMoviesData = useApiStore();
 trendingMoviesData.fetchTrendingMoviesData();
 
 const modules = [Pagination, Navigation, Autoplay];
 const router = useRouter();
+
+const changeButtonStyle = (id) => {
+  const favButton = document.getElementById(id);
+  favButton.classList.toggle('text-rose-500');
+};
 </script>
 
 <template>
@@ -55,13 +60,34 @@ const router = useRouter();
         v-for="data in trendingMoviesData.trendingWeekMoviesData.results"
         :key="data.id"
       >
-        <div>
+        <div class="relative">
           <img
             :src="trendingMoviesData.sourceOfMoviePosterUrl + data.poster_path"
             @click="router.push(`/movie/${data.id}`)"
             alt=""
             class="w-80 cursor-pointer"
           />
+          <button
+            class="btn-sm absolute bottom-2 right-2 cursor-pointer rounded-md bg-slate-900/50 text-rose-500 backdrop-blur-lg"
+            :id="`btn-favorite-${data.id}`"
+            @click="
+              () => {
+                saveToLocalStorage(data.title, {
+                  id: data.id,
+                  title: data.title,
+                  media_type: data.media_type
+                }),
+                  changeButtonStyle(`btn-favorite-${data.id}`);
+              }
+            "
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="m12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3"
+              />
+            </svg>
+          </button>
         </div>
       </swiper-slide>
     </swiper>
