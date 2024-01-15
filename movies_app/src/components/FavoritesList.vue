6fc6045f859bf0c9ef52596475e4bf05bb4c687f
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2024-01-15 13:57:19 +0300

    add trash can icon to favlist

--- movies_app/src/components/FavoritesList.vue
+++ movies_app/src/components/FavoritesList.vue
@@ -42,7 +42,13 @@
         <div class="flex w-full flex-col gap-2 pr-4">
           {{ movie.title }}
           <button @click="removeItem(movie.title)" class="btn-warning btn-sm btn w-fit self-end">
-            sil
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M7 21q-.825 0-1.412-.587T5 19V6q-.425 0-.712-.288T4 5q0-.425.288-.712T5 4h4q0-.425.288-.712T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5q0 .425-.288.713T19 6v13q0 .825-.587 1.413T17 21zm3-4q.425 0 .713-.288T11 16V9q0-.425-.288-.712T10 8q-.425 0-.712.288T9 9v7q0 .425.288.713T10 17m4 0q.425 0 .713-.288T15 16V9q0-.425-.288-.712T14 8q-.425 0-.712.288T13 9v7q0 .425.288.713T14 17"
+              />
+            </svg>
+            &nbsp; delete
           </button>
         </div>
       </li>
