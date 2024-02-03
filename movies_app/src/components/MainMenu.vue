1c13635d965425b869e9f59ecfa6fb593c7908b7
mahmutessiz <mahmutessiz71@gmail.com>
2024-02-03 13:17:07 +0300

    add fav button for mobile devices

--- movies_app/src/components/MainMenu.vue
+++ movies_app/src/components/MainMenu.vue
@@ -142,10 +142,11 @@ const switchFavorites = () => {
       </li>
     </ul>
     <div class="flex items-center gap-4 sm:hidden">
+      <!-- search bar -->
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="32"
-        height="32"
+        width="24"
+        height="24"
         viewBox="0 0 24 24"
         @click="openSearchBar"
       >
@@ -154,6 +155,9 @@ const switchFavorites = () => {
           d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5A6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14z"
         />
       </svg>
+
+      <!-- favorites button -->
+      <FavButton @click="switchFavorites" />
 
       <div class="drawer-content">
         <div @click="drawerStatusToggle">
